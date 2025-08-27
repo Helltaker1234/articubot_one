d43f6eaea892a74b8900ad36324fef1313b3527e
Helltaker1234 <ukendo3333@gmail.com>
2025-08-28 03:39:28 +0900

    feat: SLAM 성공, 다만 로봇 뒷면 아크릴이 스캔을 막아서 스캔각도 범위 지정을 해야하는데 아직 못함

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -11,10 +11,22 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+                'serial_port': '/dev/serial/by-path/pci-0000:00:14.0-usb-0:2:1.0-port0',
+                'serial_baudrate': 115200,
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
-                'scan_mode': 'Standard'
+                'scan_mode': 'Standard',
+                'angle_min': -2.094,
+                'angle_max': 2.094
+                # 에스뽀 로봇 뒷면 막혀서 스캔 범위 지정
             }]
         )
     ])
+
+
+# 우리가 쓰는 rplidar 정보
+# 포트파일명 = ttyUSB0
+# Bus 003 Device 008: ID 10c4:ea60 Silicon Labs CP210x UART Bridge
+# 
+# /dev/serial/by-path/pci-0000:00:14.0-usb-0:2:1.0-port0
+ 
