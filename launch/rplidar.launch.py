f80d7e43238c1e167e676fb45a354d410c323043
Helltaker1234 <ukendo3333@gmail.com>
2025-09-05 19:35:45 +0900

    20250905 - feat: 라이다의 스캔 범위 지정, SLAM 파라미터 조정(추후 계속 조정 필.)

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -1,8 +1,16 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+
+    # 필터 설정 파일 경로
+    filter_params_file = os.path.join(
+        get_package_share_directory('articubot_one'),
+        'config',
+        'my_laser_filter.yaml'
+    )
 
     return LaunchDescription([
 
@@ -11,16 +19,21 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/pci-0000:00:14.0-usb-0:2:1.0-port0',
+                'serial_port': '/dev/serial/by-path/pci-0000:00:14.0-usb-0:2.3:1.0-port0',
                 'serial_baudrate': 115200,
                 'frame_id': 'laser_frame',
-                'angle_compensate': True,
-                'scan_mode': 'Standard',
-                'angle_min': -2.094,
-                'angle_max': 2.094
-                # 에스뽀 로봇 뒷면 막혀서 스캔 범위 지정
+                'angle_compensate': True
             }]
-        )
+        ),
+
+        # Laser Filter 노드
+        Node(
+            package='laser_filters',
+            executable='scan_to_scan_filter_chain',
+            parameters=[filter_params_file]
+            # remappings=[('/scan', '/scan_filtered')] # 필터링된 토픽 이름을 /scan_filtered로 변경
+        ),
+
     ])
 
 
